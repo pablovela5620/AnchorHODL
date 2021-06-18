2760f5ac7300a207db07eba4caa95fceaa38f184
pablo macbookpro <pablovela5620@gmail.com>
2021-06-18 11:43:01 -0400

    update config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 mnemonic = os.environ.get('MNEMONIC', 'paste your seed here')
-target_percent = 80
-trigger_at_percent = 85
+target_percent = 75
+trigger_at_percent = 80
 
 # (True or False)
 enabled_auto_borrow = True
@@ -16,4 +16,3 @@ NOTIFY_TELEGRAM = True
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', 'paste your access token here')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', 'paste your chat id here')
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL', 'paste your webhook here')
-
